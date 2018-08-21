4f3535144ce0cd2c9ae02defc2cfbe0417835d68
Brian Stauff <brian.stauff.github@gmail.com>
2018-08-21 13:47:14 -0500

    Adding more extensions

---
+++ SqlDataProtectionProvider.Extensions/SqlDataProtectionBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace SqlDataProtectionProvider.Extensions
+{
+    public static class SqlDataProtectionBuilderExtensions
+    {
+        public static IDataProtectionBuilder PersistKeysToSqlServer(this IDataProtectionBuilder builder, string sqlConnectionString)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (string.IsNullOrEmpty(sqlConnectionString))
+            {
+                throw new ArgumentNullException(nameof(sqlConnectionString));
+            }
+
+            builder.Services.AddSingleton<IConfigureOptions<KeyManagementOptions>>(services =>
+            {
+                var loggerFactory = services.GetService<ILoggerFactory>() ?? NullLoggerFactory.Instance;
+                return new ConfigureOptions<KeyManagementOptions>(options =>
+                {
+                    options.XmlRepository = new SqlDataProtectionProvider(services.GetRequiredService<DataProtectionContext>());
+                });
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ SqlDataProtectionProvider.Extensions/SqlDataProtectionProviderServicesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SqlDataProtectionProvider.Extensions
+{
+    public static class SqlDataProtectionProviderServicesExtensions
+    {
+        public static void AddSqlDataProtectionProviderContext(this IServiceCollection services, string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            services.AddDbContext<DataProtectionContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
