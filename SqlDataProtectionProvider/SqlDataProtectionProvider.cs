d5584927dcbdd95b61c1f51ebb2ae6285bdf5dc1
Brian Stauff <brian.stauff.github@gmail.com>
2018-08-21 11:08:30 -0500

    Initial commit

---
+++ SqlDataProtectionProvider/SqlDataProtectionProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+
+namespace SqlDataProtectionProvider
+{
+    public class SqlDataProtectionProvider : IXmlRepository
+    {
+        private readonly string _connectionString;
+
+        public SqlDataProtectionProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SqlDataProtectionProviderDb/DataProtectionKeys.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[DataProtectionKeys]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [Name] nvarchar(max) NOT NULL,
+    [XmlData] nvarchar(max) NULL
+)
